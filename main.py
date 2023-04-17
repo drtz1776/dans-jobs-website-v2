eb4e12842ee231e9e22f858eb39b8f580b38a8a4
David Rutiz <118313293+drtz1776@users.noreply.github.com>
2023-04-17 02:24:06 +0000

    Added API output and multiple buttons

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-from flask import Flask
-app = Flask('app')
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+jobs = [{
+  'Job Title': "Data Engineer",
+  'Location': "Templeton",
+  'Salary': 90000
+}, {
+  'Job Title': "Data Scientist",
+  'Location': "Paso Robles",
+  'Salary': 120000
+}, {
+  'Job Title': "Data Analyst",
+  'Location': "Atascadero",
+  'Salary': 80000
+}]
+
 
 @app.route('/')
 def hello_world():
-  return 'Hello, Dan!'
+  return render_template('home.html', job=jobs)
 
-app.run(host='0.0.0.0', port=8080)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(jobs)
+
+
+#app.run(host='0.0.0.0', port=8080)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
